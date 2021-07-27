0f4ed2874ddd8b1c89b21c2b78c0bf68a61389de
The Mystic Geek <jess@themysticgeek.com>
2021-07-27 23:00:40 +0000

    Fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Use App\Http\Controllers\API\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
