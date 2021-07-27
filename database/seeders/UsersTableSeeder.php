8a333ca76c2edae640782873956dc5925c13cac9
The Mystic Geek <jess@themysticgeek.com>
2021-07-27 21:57:44 +0000

    API Changes

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,6 +15,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        
+        // Let's clear the users table first
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+        
+        // Create the dummy account
         DB::table('users')->insert([
             'name' => 'Admin Admin',
             'email' => 'admin@material.com',
@@ -22,5 +30,16 @@ class UsersTableSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+        
+        // And now let's create some more users
+        for($i = 0; $i < 10; $i++){
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password,
+            
+            ]);
+            
+        }
     }
 }
